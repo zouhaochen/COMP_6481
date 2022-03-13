29d21895544ea3f6de227ce09c5aa13145866efc
NormalZHC <zouhaochenluke@gmail.com>
2022-03-13 17:31:04 -0400

    constructor build

--- src/Assignment2/Sales.java
+++ src/Assignment2/Sales.java
@@ -1,9 +1,10 @@
 package Assignment2;
 
+import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class Sales
+public class Sales implements Serializable
 {
     //sales attributes
     private String country;
@@ -18,6 +19,64 @@ public class Sales
     private double revenue;
     private double totalCost;
     private double totalProfit;
+
+    //default constructor
+    public Sales()
+    {
+        country = "";
+        itemType = "";
+        orderPriority = Character.MIN_VALUE;
+        orderDate = null;
+        orderID = 0;
+        shipDate = null;
+        unitsSold = 0;
+        unitPrice = 0;
+        unitCost = 0;
+        revenue = 0;
+        totalCost = 0;
+        totalProfit = 0;
+    }
+
+    //constructor
+    public Sales(String country, String itemType, char orderPriority, Date orderDate,
+                 long orderID, Date shipDate, int unitsSold, float unitPrice,
+                 float unitCost, double revenue, double totalCost, double totalProfit)
+    {
+        this.country = country;
+        this.itemType = itemType;
+        this.orderPriority = orderPriority;
+        this.orderDate = orderDate;
+        this.orderID = orderID;
+        this.shipDate = shipDate;
+        this.unitsSold = unitsSold;
+        this.unitPrice = unitPrice;
+        this.unitCost = unitCost;
+        this.revenue = revenue;
+        this.totalCost = totalCost;
+        this.totalProfit = totalProfit;
+    }
+
+    //copy constructor
+    public Sales(Sales sales)
+    {
+        if(sales == null)
+        {
+            System.out.println("Fatal error");
+            System.exit(0);
+        }
+        this.country = sales.country;
+        this.itemType = sales.itemType;
+        this.orderPriority = sales.orderPriority;
+        this.orderDate = sales.orderDate;
+        this.orderID = sales.orderID;
+        this.shipDate = sales.shipDate;
+        this.unitsSold = sales.unitsSold;
+        this.unitPrice = sales.unitPrice;
+        this.unitCost = sales.unitCost;
+        this.revenue = sales.revenue;
+        this.totalCost = sales.totalCost;
+        this.totalProfit = sales.totalProfit;
+    }
 
     public void setCountry(String country)
     {
@@ -144,4 +203,32 @@ public class Sales
     {
         return totalProfit;
     }
+
+    public String toString()
+    {
+        return getCountry() + " " + getItemType() + " " + getOrderPriority() + " " + getOrderDate() + " " +
+                getOrderID() + " " + getShipDate() + " " + getUnitsSold() + " " + getUnitPrice() + " " +
+                getUnitCost() + " " + getRevenue() + " " + getTotalCost() + " " + getTotalProfit() + " ";
+    }
+
+    public boolean equals(Object object)
+    {
+        if(object == null)
+        {
+            return false;
+        }
+        else if(getClass() != object.getClass())
+        {
+            return false;
+        }
+        else
+        {
+            Sales sales = (Sales) object;
+            return (country.equals(sales.country) && itemType.equals(sales.itemType) && orderPriority == sales.orderPriority &&
+                    orderDate.equals(sales.orderDate) && orderID == sales.orderID && shipDate.equals(sales.shipDate) &&
+                    unitsSold == sales.unitsSold && unitPrice == sales.unitPrice && unitCost == sales.unitCost &&
+                    revenue == sales.revenue && totalCost == sales.totalCost && totalProfit == sales.totalProfit);
+        }
+    }
+
 }
--- src/Assignment2/SalesDatabase.java
+++ src/Assignment2/SalesDatabase.java
@@ -1,6 +1,8 @@
 package Assignment2;
 
 import java.io.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -8,7 +10,7 @@ import java.util.logging.Logger;
 
 public class SalesDatabase
 {
-    private static Sales[] salesArr;
+    private static Sales[] salesArr = new Sales[0];
     private static Logger logger = Logger.getLogger("SalesDatabase");
 
     //data folder path
@@ -18,6 +20,8 @@ public class SalesDatabase
     //buffered writer and reader
     private static BufferedWriter bufferedWriter = null;
     private static BufferedReader bufferedReader = null;
+
+    private static DateFormat format = new SimpleDateFormat("dd/mm/yyyy");
 
     public static void main(String[] args)
     {
@@ -60,6 +64,7 @@ public class SalesDatabase
                     {
                         System.out.println("\nPlease enter the option:");
                         System.out.println("1. Display file contents.");
+                        System.out.println("2. Store records from file to system.");
 
                         caseTwoChoice = keyboard.nextLine();
 
@@ -72,8 +77,18 @@ public class SalesDatabase
                                 System.out.println("/1/Tom.txt");
                                 System.out.println("/2/Sam.txt");
                                 System.out.println("/3/Harry.txt");
-                                String fileName = keyboard.nextLine();
-                                displayFileContents(fileName);
+                                String fileNameCaseOne = keyboard.nextLine();
+                                displayFileContents(fileNameCaseOne);
+                                break;
+
+                            case "2":
+                                System.out.println("\nPlease enter the file name:");
+                                System.out.println("For example:");
+                                System.out.println("/1/Tom.txt");
+                                System.out.println("/2/Sam.txt");
+                                System.out.println("/3/Harry.txt");
+                                String fileNameCaseTwo = keyboard.nextLine();
+                                storeRecord(fileNameCaseTwo);
                                 break;
                         }
                     }
@@ -255,5 +270,138 @@ public class SalesDatabase
                 }
             }
         }
+    }
+
+    //store sales records in file to array in the system
+    public static void storeRecord(String fileName)
+    {
+        File file = new File(dir, fileName);
+        int lines = 0;
+
+        //get line number of the file
+        try
+        {
+            FileReader fileReader = new FileReader(file);
+            LineNumberReader lineNumberReader = new LineNumberReader(fileReader);
+            lineNumberReader.skip(Long.MAX_VALUE);
+            lines = lineNumberReader.getLineNumber();
+            lineNumberReader.close();
+        }
+        catch (Exception e)
+        {
+            logException(e);
+            throw new RuntimeException("Failed to read file.");
+        }
+
+        //space number for store records in array
+        int recordNumber = lines+1;
+        String[] stringDataSet = new String[recordNumber];
+
+        int salesArrSpace = salesArr.length;
+        int salesArrTempSpace = salesArrSpace + recordNumber;
+        Sales[] salesArrTemp = new Sales[salesArrTempSpace];
+
+        for(int i = 0; i < salesArrSpace; i++)
+        {
+            salesArrTemp[i] = salesArr[i];
+        }
+
+        for(int i = salesArrSpace; i < salesArrTempSpace; i++)
+        {
+            salesArrTemp[i] = new Sales();
+        }
+
+        try
+        {
+            //get character stream
+            bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(file),"UTF-8"));
+            String line;
+            int index = 0;
+
+            while((line = bufferedReader.readLine()) != null)
+            {
+                stringDataSet[index] = line;
+                index = index + 1;
+            }
+
+            for(int i = 0 ; i < stringDataSet.length; i++)
+            {
+                System.out.println(stringDataSet[i]);
+                String[] splitData = stringDataSet[i].split("\\s+");
+                for (int j = 0; j < splitData.length; j++)
+                {
+                    if (j == 0)
+                    {
+                        salesArrTemp[i].setCountry(splitData[j]);
+                    }
+                    else if (j == 1)
+                    {
+                        salesArrTemp[i].setItemType(splitData[j]);
+                    }
+                    else if (j == 2)
+                    {
+                        salesArrTemp[i].setOrderPriority(splitData[j].charAt(0));
+                    }
+                    else if (j == 3)
+                    {
+                        salesArrTemp[i].setOrderDate(format.parse(splitData[j]));
+                    }
+                    else if (j == 4)
+                    {
+                        salesArrTemp[i].setOrderID(Long.parseLong(splitData[j]));
+                    }
+                    else if (j == 5)
+                    {
+                        salesArrTemp[i].setShipDate(format.parse(splitData[j]));
+                    }
+                    else if (j == 6)
+                    {
+                        salesArrTemp[i].setUnitsSold(Integer.parseInt(splitData[j]));
+                    }
+                    else if (j == 7)
+                    {
+                        salesArrTemp[i].setUnitPrice(Float.parseFloat(splitData[j]));
+                    }
+                    else if (j == 8)
+                    {
+                        salesArrTemp[i].setUnitCost(Float.parseFloat(splitData[j]));
+                    }
+                    else if (j == 9)
+                    {
+                        salesArrTemp[i].setRevenue(Double.parseDouble(splitData[j]));
+                    }
+                    else if (j == 10)
+                    {
+                        salesArrTemp[i].setTotalCost(Double.parseDouble(splitData[j]));
+                    }
+                    else if (j == 11)
+                    {
+                        salesArrTemp[i].setTotalProfit(Double.parseDouble(splitData[j]));
+                    }
+                }
+                System.out.println(salesArrTemp[i]);
+            }
+        }
+        catch(Exception e)
+        {
+            logException(e);
+            throw new RuntimeException("Failed to read file.");
+        }
+        finally
+        {
+            if(bufferedReader != null)
+            {
+                try
+                {
+                    bufferedReader.close();
+                }
+                catch(Exception e)
+                {
+                    logException(e);
+                    throw new RuntimeException("Failed to close file.");
+                }
+            }
+        }
+
     }
 }
