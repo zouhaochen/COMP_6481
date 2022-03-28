430d908488239cf33fbc1ba9c891e1eff5fc8064
NormalZHC <zouhaochenluke@gmail.com>
2022-03-28 14:03:55 -0400

    Assignment 3

---
+++ src/Assignment3/Show.java
@@ -0,0 +1,97 @@
+package Assignment3;
+
+public class Show
+{
+    private String showID;
+    private String showName;
+    private double startTime;
+    private double endTime;
+
+    //parameterized constructor accepts four values and initializes to passed values
+    Show(String showID, String showName, double startTime, double endTime)
+    {
+        this.showID = showID;
+        this.showName = showName;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    Show(Show show, String showID)
+    {
+        this.showName = show.showName;
+        this.startTime = show.startTime;
+        this.endTime = show.endTime;
+        this.showID = showID;
+    }
+
+    //mutator of show ID
+    public void setShowID(String showID)
+    {
+        this.showID = showID;
+    }
+
+    //accessor of show ID
+    public String getShowID()
+    {
+        return showID;
+    }
+
+    //mutator of show name
+    public void setShowName(String showName)
+    {
+        this.showName = showName;
+    }
+
+    //accessor of show name
+    public String getShowName()
+    {
+        return showName;
+    }
+
+    //mutator of start time
+    public void setStartTime(double startTime)
+    {
+        this.startTime = startTime;
+    }
+
+    //accessor of start time
+    public double getStartTime()
+    {
+        return startTime;
+    }
+
+    //mutator of end time
+    public void setEndTime(double endTime)
+    {
+        this.endTime = endTime;
+    }
+
+    //accessor of end time
+    public double getEndTime()
+    {
+        return endTime;
+    }
+
+    public String toString()
+    {
+        return "show ID: " + getShowID() + " show name: " + getShowName() + " start time: " + getStartTime() + " end time: " + getEndTime();
+    }
+
+    public boolean equals(Object object)
+    {
+        if(object == null)
+        {
+            return false;
+        }
+        else if(getClass() != object.getClass())
+        {
+            return false;
+        }
+        else
+        {
+            //two shows are equal with same attributes except for show ID
+            Show show = (Show) object;
+            return (showName.equals(show.showName) && startTime == show.startTime && endTime == show.endTime);
+        }
+    }
+}
