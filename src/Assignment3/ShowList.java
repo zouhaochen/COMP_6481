ab24f4c2565a052be364aa0d49e604ee3fa3c166
NormalZHC <zouhaochenluke@gmail.com>
2022-03-29 18:58:10 -0400

    Assignment 3

--- src/Assignment3/ShowList.java
+++ src/Assignment3/ShowList.java
@@ -1,5 +1,7 @@
 package Assignment3;
 
+import java.util.NoSuchElementException;
+
 public class ShowList<TVShow>
 {
     //inner class show node
@@ -83,26 +85,31 @@ public class ShowList<TVShow>
         this.head = showList.getHead();
     }
 
+    //mutator of size
     public void setSize(int size)
     {
         this.size = size;
     }
 
+    //accessor of size
     public int getSize()
     {
         return size;
     }
 
+    //mutator of head
     public void setHead(ShowNode head)
     {
         this.head = head;
     }
 
+    //accessor of head
     public ShowNode getHead()
     {
         return head;
     }
 
+    //insert the node at the head of the list
     public void addToStart(TVShow tvShow)
     {
         ShowNode showNode = new ShowNode();
@@ -120,4 +127,76 @@ public class ShowList<TVShow>
             size ++;
         }
     }
+
+    //insert the node at the last of the list
+    public void addToLast(TVShow tvShow)
+    {
+        ShowNode showNode = new ShowNode();
+        showNode = new ShowNode(tvShow, showNode);
+
+        if(head == null)
+        {
+            head = showNode;
+            size = 1;
+        }
+        else
+        {
+            findLastNode().showNode = showNode;
+            size ++;
+        }
+    }
+
+    //get the last node of the list
+    public ShowNode findLastNode()
+    {
+        ShowNode current = this.head;
+        while (current.showNode != null)
+        {
+            current = current.showNode;
+        }
+        return current;
+    }
+
+    //insert the node at the index position
+    public void insertAtIndex(TVShow tvShow, int index)
+    {
+        if(index<0||index>getSize() - 1)
+        {
+            System.exit(0);
+            throw new NoSuchElementException();
+        }
+
+        if(index == 0)
+        {
+            addToStart(tvShow);
+            return;
+        }
+
+        if(index == getSize() - 1)
+        {
+            addToLast(tvShow);
+            return;
+        }
+
+        ShowNode current = moveIndex(index);
+        ShowNode showNode = new ShowNode();
+        showNode = new ShowNode(tvShow, showNode);
+
+        showNode.showNode= current.showNode;
+        current.showNode = showNode;
+    }
+
+    //find the reference to the node at index - 1
+    public ShowNode moveIndex(int index)
+    {
+        ShowNode current = this.head;
+        int count = 0;
+        while(count != index - 1)
+        {
+            current = current.showNode;
+            count++;
+        }
+        return current;
+    }
+
 }
