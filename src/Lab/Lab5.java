a2116af91113c607fcac221ee083ee3f37949ee6
NormalZHC <zouhaochenluke@gmail.com>
2022-04-16 13:15:21 -0400

    6481 practice

--- src/Lab/Lab5.java
+++ src/Lab/Lab5.java
@@ -20,5 +20,59 @@ public class Lab5
             System.exit(0);
         }
 
+        int length = letters.length();
+
+        while (length <= string.length())
+        {
+            for(int i = 0; i < string.length() - length + 1; i ++)
+            {
+                int start = i;
+                int end = i + length - 1;
+                String subString = string.substring(start, end + 1);
+
+                char[] subStringInChar = subString.toCharArray();
+                char[] lettersInChar = letters.toCharArray();
+
+                for(int j = 0; j < subStringInChar.length; j++)
+                {
+                    char target = subStringInChar[j];
+
+                    for(int k = 0; k < lettersInChar.length; k++)
+                    {
+                        char letterInLetters = lettersInChar[k];
+
+                        if(target == letterInLetters)
+                        {
+                            lettersInChar[k] = '*';
+                            break;
+                        }
+                        else
+                            continue;
+                    }
+                }
+
+                boolean allCheck = true;
+                for(int j = 0; j <lettersInChar.length; j ++)
+                {
+                    if (lettersInChar[j] == '*')
+                    {
+
+                    }
+                    else
+                    {
+                        allCheck = false;
+                    }
+                }
+
+                if(allCheck == true)
+                {
+                    System.out.println(subString);
+                    System.exit(0);
+                }
+                else
+                    continue;
+            }
+            length ++;
+        }
     }
 }
