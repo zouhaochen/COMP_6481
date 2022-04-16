a2537542359590b2fa0cdd5d87919bf98462488e
NormalZHC <zouhaochenluke@gmail.com>
2022-04-16 10:47:50 -0400

    6481 practice

--- src/Lab/Lab3.java
+++ src/Lab/Lab3.java
@@ -1,10 +1,5 @@
 package Lab;
 
-/**
- * @Auther: Haochen Zou
- * @Date: 2022/4/15 - 下午10:51
- * @Description: Lab
- * @version: 1.0
- */
-public class Lab3 {
+public class Lab3
+{
 }
---
+++ src/Lab/Lab5.java
@@ -0,0 +1,13 @@
+package Lab;
+
+import java.util.Scanner;
+
+public class Lab5
+{
+    public static void main(String[] args)
+    {
+        Scanner keyboard = new Scanner(System.in);
+        String string = keyboard.nextLine();
+
+    }
+}
