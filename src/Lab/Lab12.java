002509178cefc0a27dd1505564177eec87f55e85
NormalZHC <zouhaochenluke@gmail.com>
2022-04-17 14:24:20 -0400

    6481 practice

--- src/Lab/Lab12.java
+++ src/Lab/Lab12.java
@@ -5,35 +5,32 @@ import java.util.Scanner;
 
 public class Lab12
 {
-    public static ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
-    public static ArrayList<Integer> list = new ArrayList<>();
-
     public static void main(String[] args)
     {
         Scanner keyboard = new Scanner(System.in);
-        String totalInString = keyboard.nextLine();
+        String firstLine = keyboard.nextLine();
         int total = 0;
 
         try
         {
-            total = Integer.parseInt(totalInString);
+            total = Integer.parseInt(firstLine);
         }
         catch (Exception e)
         {
             System.exit(0);
         }
 
-        if(total >= 1000 || total <= 0)
+        if(total >= 1000)
         {
             System.exit(0);
         }
 
-        String numberOfIntegerInString = keyboard.nextLine();
+        String secondLine = keyboard.nextLine();
         int numberOfInteger = 0;
 
         try
         {
-            numberOfInteger = Integer.parseInt(numberOfIntegerInString);
+            numberOfInteger = Integer.parseInt(secondLine);
         }
         catch (Exception e)
         {
@@ -52,27 +49,74 @@ public class Lab12
             list[i] = keyboard.nextInt();
         }
 
-        for(int i = 0; i < list.length; i ++)
+        for(int i = 0; i < list.length; i++)
         {
-            if (list[i] <= 0 || list[i] >= 100)
+            if(list[i] >= 100)
             {
                 System.exit(0);
             }
         }
 
-        for(int i = 0; i < list.length - 1; i ++)
+        int sum = 0;
+
+        for(int i = 0; i < list.length; i++)
         {
-            for(int j = i + 1; j < list.length; j ++)
+            sum = sum + list[i];
+        }
+
+        if(sum < total)
+        {
+            System.out.println(0);
+            System.exit(0);
+        }
+
+        ArrayList<Integer> dataList = new ArrayList<Integer>();
+        for(int i = 0; i < list.length; i ++)
+        {
+            dataList.add(list[i]);
+        }
+
+        int remain = total;
+        ArrayList<Integer> sumList = new ArrayList<Integer>();
+        ArrayList<String> sumInfoList = new ArrayList<String>();
+        int count = 0;
+
+        for(int i = 0; i < dataList.size(); i ++)
+        {
+            if(remain > 0)
             {
-                if(list[i] < list[j])
+                int newRemain = remain - dataList.get(i);
+                if(newRemain > 0)
                 {
-                    int temp = list[i];
-                    list[i] = list[j];
-                    list[j] = temp;
+                    sumList.add(dataList.get(i));
+                    remain = newRemain;
+                }
+                else if(newRemain == 0)
+                {
+                    sumList.add(dataList.get(i));
+                    String currentInfo = sumList.toString();
+
+                    if(sumInfoList.contains(currentInfo))
+                    {
+
+                    }
+                    else
+                    {
+                        sumInfoList.add(currentInfo);
+                    }
+
+                    sumList.clear();
+                    remain = total;
+                    i = count ++;
+                    continue;
+                }
+                else
+                {
+                    continue;
                 }
             }
         }
+
+        System.out.println(sumInfoList.size());
     }
-
-
 }
